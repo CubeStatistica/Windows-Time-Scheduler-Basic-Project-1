d63f1a28ac4f77aa892bed26458b97c2815539c8
Faizan Khalid Mohsin <faizankms8@hotmail.com>
2023-11-25 19:24:55 -0500

    Completed first draft of the python.py production code.

--- Production-Script-Update-DataCSV-File.py
+++ Production-Script-Update-DataCSV-File.py
@@ -10,8 +10,17 @@ datetime_now_str = datetime_now.strftime("%Y-%m-%d %H:%M:%S")
 # Code to determine the data size. We do not want it to be zero or very large
 newdata_size = (datetime_now.second % 22) + 1  # use modulo to now exceed size of 22.
 
+# Create the new data using np.random and the seconds in the datetime.
 df = pd.DataFrame(
     np.random.randint(0, 100, size=(newdata_size, 4)), columns=list("ABCD")
 )
 df["datetime"] = datetime_now_str
 df["seconds"] = datetime_now.second
+
+# Load existing Excel file.
+file_path = "Production-database.csv"
+
+# Pandas code to append the new rows in the dataframe to the csv file.
+# mode = 'a' specifies append mode vs. write mode.
+# header = False ensure that the header from the new data frame is not written to the csv file.
+df.to_csv(file_path, mode="a", header=False, index=False)
