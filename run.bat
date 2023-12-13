d3d69a9f2c38580d86600c413c3ccb1a03930bc5
Faizan Khalid Mohsin <faizankms8@hotmail.com>
2023-12-13 00:19:18 -0500

    Added comments in batch file.

---
+++ run.bat
@@ -0,0 +1,8 @@
+color 0a
+
+python Production-Script-Update-DataCSV-File.py
+
+::pause :: the two colons comments the line of code - but only that line of code
+
+@REM pause This is another way of commenting out but when echo is on which is the default. Can remove @ when echo is off.
+
