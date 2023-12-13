dc6e40da3e1a94a03870df70c5dc2fae08835798
Faizan Khalid Mohsin <faizankms8@hotmail.com>
2023-12-13 01:07:48 -0500

    Restored the original run.bat file with was deleted.

---
+++ run.bat
@@ -0,0 +1,9 @@
+color 0a
+
+python E:\CubeStatistica\LessonsAndBooks\Windows-Time-Scheduler-Basic-Project-1\Production-Script-Update-DataCSV-File.py
+
+::pause :: the two colons comments the line of code - but only that line of code
+
+@REM pause This is another way of commenting out but when echo is on which is the default. Can remove @ when echo is off.
+
+pause
